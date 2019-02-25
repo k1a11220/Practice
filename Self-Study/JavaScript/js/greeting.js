7ebcc115810bdfc7f08f31eaa6d995a3691f9566
Son Beomsoo <k1a11220@github.com>
2019-02-25 17:38:25 +0900

    commit master

--- Self-Study/JavaScript/js/greeting.js
+++ Self-Study/JavaScript/js/greeting.js
@@ -26,7 +26,7 @@ function askForName() {
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `안녕하세요 ${text}씨!`;
 }
 
 function loadName() {
---
+++ Self-Study/JavaScript/js/weather.js
@@ -0,0 +1,58 @@
+const API_KEY = "8f4d0ab3c9180ae0820be2bd2d43b0a4";
+
+const COORDS = 'coords';
+
+const weather = document.querySelector(".js-weather");
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        const temp = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temp} @ ${place}`
+    });
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+    console.log('앙기모띠');
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    
+    }
+}
+
+
+function init() {
+    loadCoords();
+}
+
+init();
